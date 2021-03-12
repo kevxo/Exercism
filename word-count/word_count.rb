f49e95431508348804438ab3f866221fdc041e50
Kevin Cuadros <kev040798@gmail.com>
2021-03-12 12:48:50 -0500

    Finished challenge

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -6,3 +6,23 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 
+class Phrase
+  attr_reader :phrase
+  def initialize(phrase)
+    @phrase = phrase.gsub(/'([\S]+)'/, '\1')
+  end
+
+  def word_count
+    hash = {}
+
+    @phrase.downcase.strip.split(/[^'\w]+/).each do |word|
+      if hash[word]
+        hash[word] += 1
+      else
+        hash[word] = 1
+      end
+    end
+
+    hash
+  end
+end
